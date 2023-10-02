9d7bd6a4438e05d11035049d75bf3973b46b10f2
hammamialaa <hammamialaa007@gmail.com>
2023-10-02 16:18:48 +0100

    Modification du fichier hello.c

--- hello.c
+++ hello.c
@@ -3,5 +3,6 @@ int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
    printf("hello from <dev_bilel> branch");
+   printf("hello from <dev_Alaa> branch :)");
    return 0;
 }
