c8dfe9a8d8fb8d29f0566eb0a812915204337992
rahma145 <rahma.rahali@ensi-uma.tn>
2023-10-02 11:42:02 +0100

    adding new line

--- hello.c
+++ hello.c
@@ -2,6 +2,10 @@
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
+<<<<<<< HEAD
    printf("hello from <dev_bilel> branch");
+=======
+   printf("I'm yasmine");
+>>>>>>> 25472fe (adding new line)
    return 0;
 }
