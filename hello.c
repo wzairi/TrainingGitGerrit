3314d72fd0bc53a200b9b089e1ecbd82e818de0a
rahma145 <rahma.rahali@ensi-uma.tn>
2023-10-02 14:40:55 +0100

    [jira_102] Print ASCII Value Signed-off-by: rahma145 <rahma.rahali@ensi-uma.tn>

--- hello.c
+++ hello.c
@@ -2,6 +2,10 @@
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
-   printf("I'm yasmine");
+   char c;
+   printf("Enter a character: ");
+   scanf("%c", &c);  
+   printf("ASCII value of %c = %d", c, c);    
    return 0;
 }
+  
